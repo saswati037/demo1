74dc67aa9e3e2ce2fd1d864fb0548a24e058170c
sahoo <sahoo@LAPTOP-82IU6PEL>
2022-12-12 12:53:43 +0530

    hii

---
+++ src/test/java/scripts/Test2.java
@@ -0,0 +1,13 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test2 {
+	@Test
+	public void tc2()
+	{
+		Reporter.log("hello selenium",true);
+	}
+
+}
