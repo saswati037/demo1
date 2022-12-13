0bb31c9e3bf61059c62fb8675891c3619712a2de
sahoo <sahoo@LAPTOP-82IU6PEL>
2022-12-13 12:54:10 +0530

    hii

--- src/test/java/scripts/Test5.java
+++ src/test/java/scripts/Test5.java
@@ -1,5 +1,13 @@
 package scripts;
 
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
 public class Test5 {
+	@Test
+	public void tc5()
+	{
+		Reporter.log("hello hii",true);
+	}
 
 }
